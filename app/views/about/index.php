491464aefb079bb1f19305e8eeefeef4949da926
cyptrid <cyptrid@gmail.com>
2021-01-29 19:23:45 +0700

    Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
-    <p>Halo, Nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya seorang <?= $data['pekerjaan']; ?></p>
+    <div class="container">
+        <h1>About Me</h1>
+        <img src="<?= BASEURL;  ?>/img/profile.png" alt="Ikhdanul Widad Joban" class="rounded-circle shadow" width="400">
+        <p>Halo, Nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya seorang <?= $data['pekerjaan']; ?></p>
+    <div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,2 +1,7 @@
-
-        <h1>Views</h1>
+        <div class="container">
+                <div class="bg-light mt-4">
+                        <h1>Welcome to my website!</h1>
+                        <p>...</p>
+                        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+                 </div>
+         </div>
