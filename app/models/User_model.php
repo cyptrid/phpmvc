06e2666c4255af9966b85daa1982b35fa12d8c17
cyptrid <cyptrid@gmail.com>
2021-01-29 21:24:47 +0700

    Model

---
+++ app/models/User_model.php
@@ -0,0 +1,9 @@
+<?php 
+
+class User_model{
+    private $nama = 'Ikhdan Joban';
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,7 +1,7 @@
         <div class="container">
                 <div class="bg-light mt-4">
                         <h1>Welcome to my website!</h1>
-                        <p>...</p>
+                        <p>Halo, nama saya <?= $data['nama']; ?></p>
                         <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
                  </div>
          </div>
