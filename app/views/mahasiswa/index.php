305dd3e4827e449e7692a80eccfe1c947f4e6ea4
cyptrid <cyptrid@gmail.com>
2021-01-30 05:50:11 +0700

    Insert Data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,16 +1,12 @@
-<div class="container mt-5">
+<div class="container mt-3">
+
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+    Tambah Data Mahasiswa
+    </button>
 
     <div class="row">
-        <div class="col-lg-6">
-            <?php Flasher::flash(); ?>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-        <button type="button" class="btn btn-primary" data-toogle="modal" data-target="#formModal">
-        Tambah Data Mahasiswa
-        </button>
+        <div class="col-6">
             <h3>Daftar Mahasiswa</h3>
                 <ul class="list-group">
                     <?php foreach( $data['mhs'] as $mhs ): ?>            
@@ -21,4 +17,50 @@
                 </ul>
         </div>
     </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        
+        
+        <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+            <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" id="nama" name="nama" aria-describedby="emailHelp">
+            </div>
+
+            <div class="form-group">
+                <label for="nrp">nrp</label>
+                <input type="number" class="form-control" id="nrp" name="nrp" aria-describedby="emailHelp">
+            </div>
+      
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+            </div>
+
+            <div class="form-group">
+                <label for="jurusan">Jurusan</label>
+                <select class="form-control" id="jurusan" name="jurusan">
+                       <option value="Teknik Informatika">TI</option>
+                       <option value="Sistem Informatika">SI</option>
+                </select>
+            </div>
+            </div>
+                <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Tambah Data</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
