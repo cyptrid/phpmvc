1f3b020c26a31ba9958f5f4f12ab1c318986b8a5
cyptrid <cyptrid@gmail.com>
2021-01-30 08:04:47 +0700

    Searching

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,15 +1,33 @@
 <div class="container mt-3">
 
-    <div class="ro">
+    <div class="row">
       <div class="col-lg-6">
         <?php Flasher::flash(); ?>
       </div>
     </div>
 
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
-    Tambah Data Mahasiswa
-    </button>
+    <div class="row mb-3">
+      <div class="col-lg-6">
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
+        Tambah Data Mahasiswa
+        </button>
+      </div>
+    </div>
+
+    <div class="row mb-3">
+      <div class="col-lg-6">
+        <form action="<?= BASEURL; ?>/mahasiswa/cari" method="post">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Cari Mahasiswa" name="keyword" id="keyword" autocomplete="off">
+            <div class="input-group-append">
+              <button class="btn btn-primary" type="submit" id="tombolCari">Cari</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+
 
     <div class="row">
         <div class="col-lg-6">
